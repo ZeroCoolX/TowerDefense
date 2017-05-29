555f4ac5ff2b43182918fd414bbb7823324a5db8
cnpatton <botanicality@gmail.com>
2017-05-29 10:05:11 -0500

    Added turret building logic through a BuildManager

---
+++ TowerDefense/Assets/BuildManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+    //singleton for nodes to access
+    public static BuildManager instance;
+    private void Awake() {
+        if (instance != null) {
+            Debug.LogError("More than one BuildManager - PANIC");
+        }
+        instance = this;
+    }
+
+    private GameObject _turretToBuild;
+    public GameObject turretToBuild { get { return _turretToBuild; } }
+
+    public GameObject baseTurretPrefab;
+
+    private void Start() {
+        _turretToBuild = baseTurretPrefab;
+    }
+}
---
+++ TowerDefense/Assets/Node.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Keeps track things we have built on a node, and handles user input.
+//Also checks whether the player has pressed a node, and building on the node
+public class Node : MonoBehaviour {
+
+    private Color baseColor;
+    public Color hoverColor;
+    //so it sits perfectly on the node
+    public Vector3 posOffset;
+
+    private Renderer rend;
+
+    private GameObject turret;
+
+    private void Start() {
+        rend = GetComponent<Renderer>();
+        baseColor = rend.material.color;
+    }
+
+    //user clicks on the node
+    private void OnMouseDown() {
+        if(turret != null) {
+            Debug.Log("Can't build here");//TODO: show ui for this
+            return;
+        }
+
+        //Build a turret
+        GameObject buildTurret = BuildManager.instance.turretToBuild;
+        turret = Instantiate(buildTurret, transform.position + posOffset, transform.rotation) as GameObject;
+    }
+
+    //let the user know they can interact with this node
+    private void OnMouseEnter() {
+        rend.material.color = hoverColor;
+    }
+
+    //obv
+    private void OnMouseExit() {
+        rend.material.color = baseColor;
+    }
+}
