38761ce30e3333387822fb67b079fa092e1e67ed
cnpatton <botanicality@gmail.com>
2017-05-28 20:02:33 -0500

    Added Baddies and waypoint tracking

---
+++ TowerDefense/Assets/BaddieMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaddieMovement : MonoBehaviour {
+    public float speed = 10f;   //baddie speed
+
+    private Transform target;
+    private int waypointIndex = 0;
+
+    private void Start() {
+        target = Waypoints.waypoints[0];
+    }
+
+    //move baddie closer and closer
+    private void Update() {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        //check if we reached waypoint 
+        if(Vector3.Distance(transform.position, target.position) <= 0.4f) {
+            handleNextWaypoint();
+        }
+    }
+
+    //move to next waypoint
+    private void handleNextWaypoint() {
+        if (waypointIndex >= Waypoints.waypoints.Length - 1) {
+            Destroy(gameObject);
+            return;
+        } else {
+            target = Waypoints.waypoints[++waypointIndex];
+        }
+    }
+
+}
